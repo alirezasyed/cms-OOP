5bf2c6c9eebc5e6dab22e20cd926fa5308b3d0d2
alirezasyed <codingchallenge2020@yahoo.com>
2021-03-24 00:03:28 -0700

    créer la classe createDB et établir la connexion à la base de données et également ajouter une fonction de création de table pour faire query de base de données

---
+++ admin-dashboard/DataBase.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace DataBase;
+
+use PDO;
+
+class CreateDB
+{
+    private $connection;
+    private $option = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC);
+
+
+
+    private $dbHost = "localhost";
+    private $dbName = "blog";
+    private $dbUsername = "root";
+    private $dbPassword = "";
+
+
+    function __construct()
+    {
+        try {
+            $this->connection = new PDO("mysql:host" . $this->dbHost . ";dbname=" . $this->dbName,
+            $this->dbUsername, $this->dbPassword, $this->option);
+        }
+        catch (PDOException $e) {
+            echo "<div style='color:red';>Il y a un problème de connexion :</div>" . $e->getMessage();
+        }
+    }
+
+    public function createTable($sql)
+    {
+        try {
+            $this->connection->exec($sql);
+            return true;
+        }
+        catch (PDOException $e) {
+            
+        }
+    }
+
+
+}
