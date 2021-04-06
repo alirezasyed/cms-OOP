13feb9fb38e0edd0ae308474dae18394131f946e
alirezasyed <codingchallenge2020@yahoo.com>
2021-04-06 03:25:47 -0700

    ajoute la classe de Admin et mis les redirection dedan

---
+++ admin-dashboard/Admin.class.php
@@ -0,0 +1,15 @@
+<?php
+namespace AdminDashboard;
+
+class Admin{
+    protected function redirect($url){
+        $prptocol= stripos($_SERVER['SERVER_PROTOCOL'],'https')=== true ? 'https://' : 'http://';
+        header("Location: " . $protocol . $_SERVER['HTTP_HOST'] . "/cms-php" . $url);
+
+    }
+
+
+    protected function redirectBack(){
+        header("Location: ".$_SERVER['HTTP_REFERER']);
+    }
+}
