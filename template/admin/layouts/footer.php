99f5ad7cfa052f7a2f7e8d01651d99c59561fc6c
alirezasyed <codingchallenge2020@yahoo.com>
2021-04-20 09:03:48 -0700

    cree le fichier index

--- template/admin/layouts/footer.php
+++ template/admin/layouts/footer.php
@@ -6,10 +6,10 @@
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" ></script>
 
-<script src="http://localhost/admin-panel/public/js/admin/bootstrap.min.js"></script>
+<script src="http://localhost/cms-php/public/js/admin/bootstrap.min.js"></script>
 
 <!-- MDB core JavaScript -->
-<script src="http://localhost/admin-panel/public/js/admin/mdb.min.js"></script>
+<script src="http://localhost/cms-php/public/js/admin/mdb.min.js"></script>
 
     </body>
 </html>
