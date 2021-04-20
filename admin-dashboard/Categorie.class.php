2992b5ce763b28d7849beadf6ae13115fb88275f
alirezasyed <codingchallenge2020@yahoo.com>
2021-04-20 02:56:20 -0700

    ecris le mysql query pour store edit et update

--- admin-dashboard/Categorie.class.php
+++ admin-dashboard/Categorie.class.php
@@ -22,5 +22,38 @@ class Categorie extends Admin
 
     }
 
+    public function create()
+    {
+
+    }
+
+    public function store($request, $id)
+    {
+        $db= new DataBase();
+        $db->insert('categories',array_keys($request) , $request);
+
+    }
+
+    public function edit($id)
+    {
+        $db= new DataBase();
+        $category = $db->select("SELECT * FROM `categories` WHERE `id` = ? ;", [$id])->fetch();
+
+    }
+
+    public function update($request, $id)
+    {
+        $db= new DataBase();
+        $db->update('categories',$id,array_keys($request),$request);
+
+    }
+
+    public function delete($id)
+    {
+        $db= new DataBase();
+        $db->delete('categories',$id);
+
+    }
+
 
 }
