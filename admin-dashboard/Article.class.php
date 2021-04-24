23c414ecbf296d86e348fb3d084a846e4c16ac6b
alirezasyed <codingchallenge2020@yahoo.com>
2021-04-23 22:10:07 -0700

    ajoute la class Articles

---
+++ admin-dashboard/Article.class.php
@@ -0,0 +1,50 @@
+<?php 
+
+namespace AdminDashboard;
+
+
+require_once ("Admin.class.php");
+require_once (realpath(dirname(__FILE__) . "/DataBase.php"));
+
+
+use DataBase\DataBase;
+
+
+class Article {
+    
+    public function index()
+    {
+
+    }
+
+    public function show($id)
+    {
+
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function store($request)
+    {
+
+    }
+
+    public function edit($id)
+    {
+
+    }
+
+    public function update($request, $id)
+    {
+
+    }
+
+    public function delete($id)
+    {
+        
+    }
+
+}
