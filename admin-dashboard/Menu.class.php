722c6505a4e1c1e7c0519ea9924cc0cb3035a23f
alirezasyed <codingchallenge2020@yahoo.com>
2021-05-30 16:29:43 -0700

    ajoute les queries d'index, show, create  de menu

--- admin-dashboard/Menu.class.php
+++ admin-dashboard/Menu.class.php
@@ -3,22 +3,32 @@ namespace AdminDashboard;
 
 require_once "Admin.class.php";
 require_once realpath(dirname(__FILE__) . "/DataBase.php");
+use DataBase\DataBase;
 
 class Menu extends Admin
 {
 
     public function index()
     {
+        $db = new DataBase();
+        $menus = $db->select('SELECT * FROM `menus` ORDER BY `id` DESC ;');
+        require_once realpath(dirname(__FILE__) . "/../template/admin/menus/index.php");
 
     }
 
     public function show($id)
     {
+        $db = new DataBase();
+        $menu = $db->select("SELECT * FROM `menus` WHERE (`id` = ?); ", [$id])->fetch();
+        require_once realpath(dirname(__FILE__) . "/../template/admin/menus/show.php");
 
     }
 
     public function create()
     {
+        $db = new DataBase();
+        $menus = $db->select("SELECT * FROM `menus` WHERE `parent_id` IS NULL ;");
+        require_once realpath(dirname(__FILE__) . "/../template/admin/menus/create.php");
 
     }
 
@@ -32,10 +42,12 @@ class Menu extends Admin
 
     public function update($request, $id)
     {
+
     }
 
     public function delete($id)
     {
+
     }
 
 }
