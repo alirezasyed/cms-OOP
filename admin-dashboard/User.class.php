fdb627823420cc25eb8732bffbada901f56159ee
alirezasyed <codingchallenge2020@yahoo.com>
2021-06-16 12:01:18 -0700

    ajoute le fichier auth et écris ses fonctions de login

--- admin-dashboard/User.class.php
+++ admin-dashboard/User.class.php
@@ -7,27 +7,33 @@ require_once ("Admin.class.php");
  class User extends Admin{
 
     public function index(){
+
         $db= new DataBase();
         $users=$db->select('SELECT * FROM `users` ORDER BY `id` DESC ;');
         require_once (realpath(dirname(__FILE__). "/../template/admin/users/index.php"));
     }
 
     public function permission($id){
+
         $db= new DataBase();
         $user = $db->select("SELECT * FROM `users` WHERE (`id` = ?); ",[$id])->fetch();
         if($user['permission'] == 'admin'){
+
             $db->update('users',$id,['permission'],['user']);
 
         }
         else{
+
             $db->update('users',$id,['permission'],['admin']);
 
         }
+        
         $this->redirectBack();
 
     }
 
     public function edit($id){
+
         $db= new DataBase();
         $user = $db->select("SELECT * FROM `users` WHERE `id` = ? ;", [$id])->fetch();
         require_once (realpath(dirname(__FILE__). "/../template/admin/users/edit.php"));
@@ -35,12 +41,14 @@ require_once ("Admin.class.php");
     }
 
     public function update($request,$id){
+
         $db= new DataBase();
         $db->update('users',$id,array_keys($request),$request);
         $this->redirect('user');
     }
 
     public function delete($id){
+        
         $db= new DataBase();
         $db->delete('users',$id);
         $this->redirectBack();
