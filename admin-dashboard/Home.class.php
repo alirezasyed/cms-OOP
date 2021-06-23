f1785926dec586f155a03c9593a76f8efb3f3b80
alirezasyed <codingchallenge2020@yahoo.com>
2021-06-23 10:15:06 -0700

    écris la requête pour les menus et sous-menus

--- admin-dashboard/Home.class.php
+++ admin-dashboard/Home.class.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace AdminDashboard;
 
 require_once(realpath(dirname(__FILE__) . "/DataBase.php"));
@@ -13,6 +14,14 @@ class Home {
         $popularArticles = $db->select("SELECT articles.*, (SELECT COUNT(*) FROM comments WHERE comments.article_id = articles.id) AS comments_count, (SELECT username FROM users WHERE users.id = articles.user_id) AS username FROM articles  ORDER BY `view` DESC LIMIT 0,4 ;")->fetchAll();
 
         $sidebarPopularArticles = $popularArticles;
+
+        $categories = $db->select('SELECT * FROM `categories` ORDER BY `id` DESC ;');
+
+        $menus = $db->select('SELECT *, (SELECT COUNT(*) FROM `menus` AS `submenus` WHERE `submenus`.`parent_id` = `menus`.`id`  ) as `submenu_count`  FROM `menus` WHERE `parent_id` IS NULL ;')->fetchAll();
+        
+        $submenus = $db->select('SELECT * FROM `menus` WHERE `parent_id` IS NOT NULL ;')->fetchAll();
+
+        require_once (realpath(dirname(__FILE__) . "/../template/app/index.php"));
     }
 
     public function show($id){
