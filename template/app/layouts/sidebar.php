d4ac04eaf3bf69e207ebc9d667ca11a418ff0952
alirezasyed <codingchallenge2020@yahoo.com>
2021-07-04 00:31:06 -0700

    ajoute le sidebar dans layouts

---
+++ template/app/layouts/sidebar.php
@@ -0,0 +1,34 @@
+<aside class="sidebar">
+
+    <div class="sidebar-item">
+        <ul class="sidebar-list">
+            <li><a href="http://localhost/cms-php/show-category/1"><b>Lorem ipsum dolor.</b></a></li>
+        </ul>
+    </div>
+
+    <div class="sidebar-item">
+
+        <h2 class="title">POPULAR POSTS</h2>
+
+            <div class="popular-post">
+
+                <img class="popular-post-img" src="http://localhost/cms-php/4" alt="">
+
+                <div class="popular-post-title">
+                    <h3>
+                        <a href="http://localhost/cms-php/show-article/5"><b>Lorem ipsum dolor.</b></a>
+                    </h3>
+                    <ul class="info-bar">
+                        <li class=""><span class="text-muted">by</span> <a href="#" class="color-black"><b>Kam,</b></a>
+                            <span class="text-muted">feb 4 2021</span></li>
+                    </ul>
+                </div>
+
+                <div class="clear-fix"></div>
+
+            </div>
+
+    </div>
+
+
+</aside>
