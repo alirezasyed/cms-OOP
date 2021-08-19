d69e635eedf63fd1436c8678e0e0d070abc7fc76
alirezasyed <codingchallenge2020@yahoo.com>
2021-08-19 10:57:04 -0700

    je rends la header dynamique pour voir les unseen commentaires

--- template/admin/layouts/head-tag.php
+++ template/admin/layouts/head-tag.php
@@ -2,11 +2,18 @@
 <html lang="en">
 <head>
 
+<?php
+if(!isset($db)){
+    $db= new \DataBase\DataBase();
+} 
+$setting= $db->select("SELECT * FROM `websetting`;")->fetch();
+?>
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title></title>
-    <link rel="shortcut icon" href="http://localhost/cms-php/" type="image/x-icon" />
+    <title><?php echo $setting['title'] ?></title>
+    <link rel="shortcut icon" href="http://localhost/cms-php/<?php echo $setting['icon'] ?>" type="image/x-icon" />
 
     <link rel="stylesheet" href="http://localhost/cms-php/public/css/admin/bootstrap.min.css" type="text/css">
 
@@ -24,31 +31,56 @@
 
     <nav class="navbar  navbar-light bg-gradiant-green-blue nav-shadow">
 
-        <a class="navbar-brand" href="http://localhost/cms-php/admin"></a>
+        <a class="navbar-brand" href="http://localhost/cms-php/admin"><?php echo $setting['title'] ?></a>
         <span class="">
 
-                    <a class="text-decoration-none px-3 text-dark" href="http://localhost/cms-php/comment"><i class="fas fa-comments"></i> <span class="badge badge-danger"></span> </a>
+        <?php
+                $unseenComments = $db->select("SELECT COUNT(*) FROM `comments` WHERE `status` = 'unseen' ;")->fetch();
+                $username = $db->select("SELECT * FROM `users` WHERE( `id` = '".$_SESSION['user']."') ;")->fetch();
+
+        ?>
+
+                    <a class="text-decoration-none px-3 text-dark" href="http://localhost/cms-php/comment"><i class="fas fa-comments"></i><?php if($unseenComments['COUNT(*)']) {?><span class="badge badge-danger"><?php echo $unseenComments['COUNT(*)'] ?> new!!!</span><?php }?></a>
+
                 <span class="dropdown">
+
                     <a class="dropdown-toggle text-decoration-none text-dark" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+
                         <i class="fas fa-user"></i>
+
+                        <?php echo $username['username'] ?>
+
                     </a>
+
                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+
                         <a class="dropdown-item" href="http://localhost/cms-php/logout">logout</a>
+
                     </div>
+
                 </span>
+
         </span>
+
     </nav>
 
     <div class="container-fluid">
         <div class="row">
             <nav class="col-md-2 d-none d-md-block pt-3 bg-sidebar sidebar px-0">
                 <a class="text-decoration-none d-block py-1 px-2 mt-1" href="http://localhost/cms-php/admin"><i class="fas fa-home"></i> Home</a>
+                
                 <a class="text-decoration-none d-block py-1 px-2 mt-1" href="http://localhost/cms-php/category"><i class="fas fa-clipboard-list"></i> Category</a>
+                
                 <a class="text-decoration-none d-block py-1 px-2 mt-1" href="http://localhost/cms-php/article"><i class="fas fa-newspaper"></i> Article</a>
+                
                 <a class="text-decoration-none d-block py-1 px-2 mt-1" href="http://localhost/cms-php/comment"><i class="fas fa-comments"></i> Comment</a>
+                
                 <a class="text-decoration-none d-block py-1 px-2 mt-1" href="http://localhost/cms-php/menu"><i class="fas fa-bars"></i> Menus</a>
+                
                 <a class="text-decoration-none d-block py-1 px-2 mt-1" href="http://localhost/cms-php/user"><i class="fas fa-users"></i> User</a>
+                
                 <a class="text-decoration-none d-block py-1 px-2 mt-1" href="http://localhost/cms-php/web-setting"><i class="fas fa-tools"></i> Web Setting</a>
+                
             </nav>
 
             <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
